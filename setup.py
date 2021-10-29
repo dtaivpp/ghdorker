10ca3bf0a6f2111ad70b2c74c9aa126a57cbd6fc
David Tippett <dtaivpp@gmail.com>
2021-10-29 14:43:30 -0400

    Major Version Bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ghdorker",
-    version='0.1.0',
+    version='0.2.0',
     author="David Tippett",
     author_email="dtaivpp@gmail.com",
     description="A better GitHub Dorking Utility",
